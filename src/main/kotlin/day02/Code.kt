6a575ba705f9f047a37cfa57af6d094efa06d772
fcolasuonno <fcolasuonno@gmail.com>
2021-12-02 10:03:23 +0000

    Day 2

--- src/main/kotlin/day02/Code.kt
+++ src/main/kotlin/day02/Code.kt
@@ -13,16 +13,49 @@ fun main() {
     part2(parsed)
 }
 
+enum class Direction(val horizontal: Int, val depth: Int) {
+    FORWARD(1, 0),
+    DOWN(0, 1),
+    UP(0, -1),
+}
+
 fun parse(input: List<String>) = input.map {
-    it.toInt()
+    it.split(' ').let { (movement, amount) ->
+        requireNotNull(
+            when (movement) {
+                "forward" -> Direction.FORWARD
+                "down" -> Direction.DOWN
+                "up" -> Direction.UP
+                else -> null
+            }
+        ) to amount.toInt()
+    }
 }.requireNoNulls()
 
-fun part1(input: List<Int>) {
-    val res = input.zipWithNext().count { it.second > it.first }
+fun part1(input: List<Pair<Direction, Int>>) {
+    val res =
+        input.fold(Pair(0, 0)) { (horizontal, depth), (dir, amount) ->
+            Pair(
+                (horizontal + dir.horizontal * amount),
+                (depth + dir.depth * amount).coerceAtLeast(0)
+            )
+        }.let { (x, y) ->
+            x * y
+        }
     println("Part 1 = $res")
 }
 
-fun part2(input: List<Int>) {
-    val res = input.windowed(3).map { it.sum() }.zipWithNext().count { it.second > it.first }
+fun part2(input: List<Pair<Direction, Int>>) {
+    val res = input.fold(Triple(0, 0, 0)) { (aim, horizontal, depth), (dir, amount) ->
+        Triple(
+            aim + when (dir) {
+                Direction.FORWARD -> 0
+                Direction.DOWN -> amount
+                Direction.UP -> -amount
+            },
+            horizontal + if (dir == Direction.FORWARD) amount else 0,
+            (depth + if (dir == Direction.FORWARD) aim * amount else 0).coerceAtLeast(0)
+        )
+    }.let { (_, horizontal, depth) -> horizontal * depth }
     println("Part 2 = $res")
 }
