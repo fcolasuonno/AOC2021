6cd53dfd71f65c9846186188d7e69b14ddba1091
fcolasuonno <fcolasuonno@gmail.com>
2021-12-03 11:10:17 +0000

    Day 3

--- src/main/kotlin/day03/Code.kt
+++ src/main/kotlin/day03/Code.kt
@@ -13,16 +13,41 @@ fun main() {
     part2(parsed)
 }
 
-fun parse(input: List<String>) = input.map {
-    it.toInt()
+fun parse(input: List<String>) = input.map { line ->
+    line.map { if (it == '0') -1 else 1 }
 }.requireNoNulls()
 
-fun part1(input: List<Int>) {
-    val res = input
+fun List<Int>.toDec() = map { if (it > 0) 1 else 0 }.fold(0) { acc, i -> acc * 2 + i }
+
+fun <T : Any> generateSequenceIndexed(seed: T, nextFunction: (Int, T) -> T?) =
+    generateSequence(Pair(0, seed)) { (index, current) ->
+        nextFunction(index, current)?.let {
+            Pair(
+                index + 1,
+                it
+            )
+        }
+    }.map { it.second }
+
+fun <T : Any> Sequence<List<T>>.firstSingle() = first { it.size == 1 }.single()
+
+fun part1(input: List<List<Int>>) {
+    val sum = input.reduce { acc, ints -> acc.zip(ints) { a, b -> a + b } }
+    val gamma = sum.toDec()
+    val epsilon = sum.map { -it }.toDec()
+    val res = epsilon * gamma
     println("Part 1 = $res")
 }
 
-fun part2(input: List<Int>) {
-    val res = input
+fun part2(input: List<List<Int>>) {
+    val oxygen = generateSequenceIndexed(input) { index, list ->
+        val bitFilter = if (list.sumOf { it[index] } < 0) -1 else 1
+        list.filter { it[index] == bitFilter }
+    }.firstSingle().toDec()
+    val scrubber = generateSequenceIndexed(input) { index, list ->
+        val bitFilter = if (list.sumOf { it[index] } >= 0) -1 else 1
+        list.filter { it[index] == bitFilter }
+    }.firstSingle().toDec()
+    val res = oxygen * scrubber
     println("Part 2 = $res")
 }
---
+++ src/main/kotlin/day04/Code.kt
@@ -0,0 +1,28 @@
+package day04
+
+import isDebug
+import java.io.File
+
+fun main() {
+    val name = if (isDebug()) "test.txt" else "input.txt"
+    System.err.println(name)
+    val dir = ::main::class.java.`package`.name
+    val input = File("src/main/kotlin/$dir/$name").readLines()
+    val parsed = parse(input)
+    part1(parsed)
+    part2(parsed)
+}
+
+fun parse(input: List<String>) = input.map {
+    it.toInt()
+}.requireNoNulls()
+
+fun part1(input: List<Int>) {
+    val res = input
+    println("Part 1 = $res")
+}
+
+fun part2(input: List<Int>) {
+    val res = input
+    println("Part 2 = $res")
+}
